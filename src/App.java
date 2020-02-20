b435c402c5eb8a42583cb061e0e75b4e3f8a5a32
swen922 <swen922@gmail.com>
2020-02-20 16:52:45 +0400

    PatternFlyweight

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import tree.Tree;
 
+/** Легковес, также известен как: Приспособленец, Кэш, Flyweight */
+
 public class App {
     public static void main(String[] args) {
         Forest forest = new Forest();
